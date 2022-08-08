1847bea81f6ec0d8409e149e980d14c05f63cf75
AUGMENTARO <109080715+AUGMENTARO@users.noreply.github.com>
2022-08-08 22:33:01 +1000

    Update script.js

--- Новая папка (2)/script.js
+++ Новая папка (2)/script.js
@@ -1,8 +1,8 @@
 const containerElem = document.querySelector('.container');
 
-const renderChess = (boardSize) => {
+const renderChessBoard = (boardSize) => {
     for (let i = 0; i < (boardSize ** 2); i++) {
-        const cardElem = document.createElement("div");
+        const cardElem = document.createElement('div');
         cardElem.classList.add('card');
         containerElem.append(cardElem);
     }
