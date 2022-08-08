263f83c418a7b2154d624f654b3f7263b4eecbf6
AUGMENTARO <109080715+AUGMENTARO@users.noreply.github.com>
2022-08-08 22:39:33 +1000

    Update script.js

--- Новая папка (2)/script.js
+++ Новая папка (2)/script.js
@@ -3,6 +3,7 @@ const containerElem = document.querySelector('.container');
 const renderChessBoard = (boardSize) => {
     containerElem.style.width = boardSize * 100 + 2 + 'px';
     containerElem.style.height = boardSize * 100 + 2 + 'px';
+
     for (let i = 0; i < (boardSize ** 2); i++) {
         const cardElem = document.createElement('div');
         cardElem.classList.add('card');
