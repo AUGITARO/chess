9f6d605c4618311e73b221f2620be61dd7226071
AUGMENTARO <109080715+AUGMENTARO@users.noreply.github.com>
2022-08-10 00:31:24 +1000

    Update script.js

--- Новая папка (2)/script.js
+++ Новая папка (2)/script.js
@@ -4,10 +4,17 @@ const renderChessBoard = (boardSize) => {
     containerElem.style.width = boardSize * 100 + 2 + 'px';
     containerElem.style.height = boardSize * 100 + 2 + 'px';
 
-    for (let i = 0; i < (boardSize ** 2); i++) {
-        const cardElem = document.createElement('div');
-        cardElem.classList.add('card');
-        containerElem.append(cardElem);
+    for (let row = 0; row < boardSize; row++) {
+        for (let cell = 0; cell < boardSize; cell++) {
+            const cardElem = document.createElement('div');
+            cardElem.classList.add('card');
+            containerElem.append(cardElem);
+            if (((row % 2) === 0) && ((cell % 2) !== 0)) {
+                cardElem.style.backgroundColor = 'black';
+            } else if (((row % 2) !== 0) && ((cell % 2) === 0)) {
+                cardElem.style.backgroundColor = 'black';
+            }
+        }
     }
 }
 
